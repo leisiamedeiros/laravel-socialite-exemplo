2ef6244bbd35a8feeb047ca29d69e439b7cddf0b
Leisia Medeiros <leisiamedeiros@gmail.com>
2017-11-05 17:29:59 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simples exemplo de usagem do socialite.
+
+
+
+> Um simples exemplo da utilização do sicialite usando o provider do github.
+
+#### Arquivos alterados:
+* .env.example
+* routes\web.php
+* config\app.php
+* config\services.php
+* database\migrations\user_table
+* app\User.php —filable=provider_id
+* App\Http\Controllers\Auth\LoginController
---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+use Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('github')->user();
+        //return response()->json($user);
+
+        $authUser = $this->findOrCreateUser($user);
+        Auth::login($authUser, true);
+        return redirect($this->redirectTo);
+    }
+
+    /**
+     * If a user has registered before using social auth, return the user
+     * else, create a new user object.
+     * @param  $user Socialite user object
+     * @param $provider Social auth provider
+     * @return  User
+     */
+    public function findOrCreateUser($user)
+    {
+        $authUser = User::where('provider_id', $user->id)->first();
+        if (!empty($authUser)) {
+            return $authUser;
+        } else {
+            return User::create([
+                'name'     => $user->name,
+                'email'    => $user->email,
+                'password' => $user->token,
+                'provider_id' => $user->id,
+            ]);
+        }
+    }
+}
